6aff6e2602ecde8e10d78b9ab3fc53f894a0a7fb
sxf <simonshi387@gmail.com>
2019-10-01 10:08:14 -0400

    Lab3

--- app/src/main/java/com/example/androidlabs/FirstActivity.java
+++ app/src/main/java/com/example/androidlabs/FirstActivity.java
@@ -1,7 +1,9 @@
 package com.example.androidlabs;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -9,41 +11,42 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FirstActivity extends AppCompatActivity {
+    private EditText editText;
+    private Button logInButton;
+    private SharedPreferences prefs;
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
-
-        //find button 1 from what was loaded, add a click listener
-        /*Button page1Button = findViewById(R.id.firstButton);
-        if(page1Button != null)
-            page1Button.setOnClickListener(v -> {
-                Intent goToPage2 = new Intent(FirstActivity.this, SecondActivity.class);
-                    startActivity(goToPage2);
-            });
-*/
-        EditText editText = findViewById(R.id.emailInput);
+        editText = findViewById(R.id.emailInput);
+        logInButton = findViewById(R.id.loginButton);
+        prefs = getSharedPreferences("Lab3", MODE_PRIVATE);
+        String previous = prefs.getString("emailInput", "");
+        //if(!TextUtils.isEmpty(previous))
+        editText.setText(previous);
 
         //use a Lambda function to set a click listener
-        Button page1Button = (Button)findViewById(R.id.loginButton);
-        if(page1Button != null){
-            page1Button.setOnClickListener(clk -> {
+
+        if(logInButton != null){
+            logInButton.setOnClickListener(clk -> {
                 Intent goToPage2 = new Intent(FirstActivity.this, SecondActivity.class);
                 goToPage2.putExtra("emailInput",editText.getText().toString());
 
-               startActivity(goToPage2);
+                startActivity(goToPage2);
                // startActivityForResult(goToPage2, 30);
             });
         }
 
     }
-
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("emailInput",editText.getText().toString());
+        editor.apply();
+    }
     @Override
     protected void onStart() {
         super.onStart();
